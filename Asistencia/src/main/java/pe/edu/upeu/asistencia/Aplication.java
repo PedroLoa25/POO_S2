045d9ad73d9478c6ecfe77e8a46af7bb73ecc546
PedroLoa25 <pfloayza1704@gmail.com>
2025-08-22 19:50:01 -0500

    hola

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -1,4 +1,8 @@
 package pe.edu.upeu.asistencia;
 
 public class Aplication {
+    public static void main(String[] args) {
+        AsistenciaApplication.main(args);
+        System.out.println("Iniciando aplicacion del papu");
+    }
 }
