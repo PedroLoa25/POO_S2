aa3f60785a1fa6b3ea3b093a0fe991d267260b1f
PedroLoa25 <pfloayza1704@gmail.com>
2025-08-29 16:07:46 -0500

    conceptos POO added

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/servicio/EstudianteServicioImp.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.asistencia.servicio;
+
+import pe.edu.upeu.asistencia.repositorio.EstudianteRepository;
+
+public class EstudianteServicioImp extends EstudianteRepository implements EstudianteServicioI {
+
+}
