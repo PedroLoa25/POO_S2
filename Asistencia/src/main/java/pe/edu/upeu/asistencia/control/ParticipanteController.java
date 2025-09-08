cc2e354bebeb01916fda5987884163db77dc9f1e
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-09-08 11:31:50 -0500

    added

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
@@ -1,17 +1,21 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import pe.edu.upeu.asistencia.enums.Carrera;
 import pe.edu.upeu.asistencia.enums.TipoParticipante;
 import pe.edu.upeu.asistencia.modelo.Participante;
 import pe.edu.upeu.asistencia.servicio.ParticipanteServicioI;
+
+import javax.swing.*;
 
 @Controller
 public class ParticipanteController {
@@ -22,32 +26,66 @@ public class ParticipanteController {
     private ComboBox<TipoParticipante> cbxTipoParticipante;
 
     @FXML
+    private TextField txtDni, txtNombres, txtApellidos;
+
+    @FXML
     private TableView<Participante> tableRegPart;
     ObservableList<Participante> participantes;
 
     @Autowired
     ParticipanteServicioI ps;
 
+    TableColumn<Participante, String> dniCol, nombreCol, apellidosCol, carreraCol, tipoParticipanteCol;
+
     @FXML
     public void initialize(){
         cbxCarrera.getItems().addAll(Carrera.values());
         cbxTipoParticipante.getItems().addAll(TipoParticipante.values());
 
-        cbxCarrera.getSelectionModel().select(4);
+        cbxCarrera.getSelectionModel().select(1);
         Carrera carrera = cbxCarrera.getSelectionModel().getSelectedItem();
         System.out.println(carrera.name());
+        definirColumnas();
         listarParticiantes();
     }
 
+    public void limpiarFormularios(){
+        txtDni.setText("");
+        txtNombres.setText("");
+        txtApellidos.setText("");
+        cbxCarrera.getSelectionModel().clearSelection();
+        cbxTipoParticipante.getSelectionModel().clearSelection();
+    }
+
+    @FXML
+    public void registrarParticipante(){
+        Participante p = new Participante();
+        p.setDni(new SimpleStringProperty(txtDni.getText()));
+        p.setNombre(new SimpleStringProperty(txtNombres.getText()));
+        p.setApellidos(new SimpleStringProperty(txtApellidos.getText()));
+        p.setCarrera(cbxCarrera.getSelectionModel().getSelectedItem());
+        p.setTipoParticipante(cbxTipoParticipante.getSelectionModel().getSelectedItem());
+        ps.save(p);
+        limpiarFormularios();
+        listarParticiantes();
+    }
+
+    public void definirColumnas(){
+        dniCol = new TableColumn<>("DNI");
+        nombreCol = new TableColumn<>("Nombre");
+        apellidosCol = new TableColumn<>("Apellidos");
+        carreraCol = new TableColumn<>("Carrera");
+        tipoParticipanteCol = new TableColumn<>("Tipo Participante");
+        tableRegPart.getColumns().addAll(dniCol,nombreCol, apellidosCol, carreraCol, tipoParticipanteCol);
+    }
+
     public void listarParticiantes(){
-        TableColumn<Participante, String> dniCol = new TableColumn<>("DNI");
         dniCol.setCellValueFactory(cellData -> cellData.getValue().getDni());
-
-        TableColumn<Participante, String> nombreCol = new TableColumn<>("Nombre");
         nombreCol.setCellValueFactory(cellData -> cellData.getValue().getNombre());
-
+        apellidosCol.setCellValueFactory(cellData -> cellData.getValue().getApellidos());
+        carreraCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCarrera().toString()));
+        tipoParticipanteCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTipoParticipante().name()));
         participantes = FXCollections.observableList(ps.findAll());
-        tableRegPart.getColumns().addAll(dniCol,nombreCol);
         tableRegPart.setItems(participantes);
     }
 }
