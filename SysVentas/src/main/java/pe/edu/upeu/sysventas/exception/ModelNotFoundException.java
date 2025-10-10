ed55c54f9699aec08b332c5819a6ee77832917cc
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-10-10 16:16:42 -0500

    iniciando service

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/exception/ModelNotFoundException.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/exception/ModelNotFoundException.java
@@ -3,14 +3,14 @@ package pe.edu.upeu.sysventas.exception;
 import lombok.Getter;
 
 @Getter
-public class ModelNotFounException extends RuntimeException {
+public class ModelNotFoundException extends RuntimeException {
     private final int errorCode;
-    public ModelNotFounException(String message){
+    public ModelNotFoundException(String message){
         super(message);
         this.errorCode = 0;
     }
 
-    public ModelNotFounException(String message, int errorCode){
+    public ModelNotFoundException(String message, int errorCode){
         super(message);
         this.errorCode = errorCode;
     }
