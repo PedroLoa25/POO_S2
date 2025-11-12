1a021687214cab0f1fdfaca1d2c3c7929e54693d
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-11-11 19:25:11 -0500

    reporte integrado

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/service/impl/VentaServiceImp.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/service/impl/VentaServiceImp.java
@@ -16,6 +16,7 @@ import javax.sql.DataSource;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 
@@ -63,6 +64,8 @@ public class VentaServiceImp extends CrudGenericServiceImp<Venta, Long> implemen
         JasperDesign jdesign = JRXmlLoader.load(getFile("comprobante.jrxml"));
         JasperReport jreport = JasperCompileManager.compileReport(jdesign);
         // Llenar el informe
-        return JasperFillManager.fillReport(jreport, param, dataSource.getConnection());
+        try(Connection conn = dataSource.getConnection()){
+            return JasperFillManager.fillReport(jreport, param, conn);
+        }
     }
 }
